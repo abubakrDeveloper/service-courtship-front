91abdd710786c951e7413a7c16f60dbdb95a7c04
userFrontend <aba06096@gmail.com>
2025-08-30 14:50:07 +0500

    Hali AppLayout ochmagansan ?

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import App from './App.jsx'
-import { router } from './router/index';
-import 'antd/dist/reset.css';
+import App from "./App.jsx";
+import { router } from "./router/index";
+import "antd/dist/reset.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <RouterProvider router={router} />
     <App />
-  </React.StrictMode>,
-)
+  </>
+);
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,15 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
 import Register from "../pages/register/register";
+import Login from "../pages/Login/Login";
 
 export const router = createBrowserRouter([
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> },
-  {
-    path: "/",
-    element: <AppLayout />,
-    children: [
-      { index: true, element: <Dashboard /> },
-      { path: "products", element: <Products /> },
-      { path: "stats", element: <Stats /> },
-    ],
-  },
+  // {
+  //   path: "/",
+  //   element: <AppLayout />,
+  //   children: [
+  //     { index: true, element: <Dashboard /> },
+  //     { path: "products", element: <Products /> },
+  //     { path: "stats", element: <Stats /> },
+  //   ],
+  // },
 ]);
