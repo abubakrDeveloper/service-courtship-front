b0fdebf5a751b1ac26eb9923f1c774aab32fdfc5
abubakrDeveloper <abubakrrashidov42@gmail.com>
2025-09-26 15:31:27 +0500

    main.js ichidagini qaytardim kotta xoatoik bo'lib qolibti

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
+
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
+import { MemoryRouter } from "react-router-dom"; 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
